e292e9324390a016d509171459e11b1e7eb198ff
merry <159569959+merrypranxter@users.noreply.github.com>
2026-09-25 17:57:44 -0500

    Mouth Lab Job 2: export breeder systems

--- src/mouthLab/index.ts
+++ src/mouthLab/index.ts
@@ -2,3 +2,7 @@ export * from './types';
 export * from './traits';
 export * from './donors';
 export * from './objectives';
+export * from './conflictMatrix';
+export * from './determinism';
+export * from './phenotype';
+export * from './breeder';
